dba78d5e605a368284b8b434414210de71d476e7
leerehgit <eun6546@naver.com>
2022-10-11 18:30:19 +0900

    feat: Do fibo with recursion + mem

--- fibo.py
+++ fibo.py
@@ -3,8 +3,20 @@ def fibo(n):
         return n
     else:
         return fibo(n-1) + fibo(n-2)
+
+def fibo_mem():
+    cache = {
+        0:0,
+        1:1
+    }
+    def func(n):
+        if n not in cache:
+            cache[n] = func(n-1) + func(n-2)
+        return cache[n]
+
 if __name__ == '__main__':
     user_num = int(input('Enter num: '))
+    fibo_m = fibo_mem()
     print(fibo(user_num))
 
 
